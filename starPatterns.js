37b003f019788a5e82f26c8dbd6c68dd55ff1c02
maheshraju1218@gmail.com <maheshraju1218@gmail.com>
2023-06-10 23:47:26 +0530

    basic coding

--- starPatterns.js
+++ starPatterns.js
@@ -1,3 +1,4 @@
+//print stars for given input
 function printStar(n){
   let pattern = ''
   for(let i= 0; i<n; i++){
@@ -5,6 +6,8 @@ function printStar(n){
   }
   console.log(pattern)
 }
+
+// print left pyramid
 
 function leftPyramid(n) {
  for (let i = 1; i <= n; i++) {
@@ -25,17 +28,47 @@ const res2 = printStar(3);
 console.log(res2);
 
 
+// Sum all the numbers for a given range
+function rangeSum(start,end){
+  let sum = 0;
+  for(let i=start; i<end; i++){
+      sum+=i;
+  }
+  return sum;
+}
+const rangeSumRes = rangeSum(1,10);
+console.log(rangeSumRes);
+
+
+function fibonacci(n) {
+  let fibonacciSeries = [0, 1]; 
+
+  // Generate the Fibonacci series
+  for (let i = 2; i < n; i++) {
+    let nextNum = fibonacciSeries[i - 1] + fibonacciSeries[i - 2];
+    fibonacciSeries.push(nextNum);
+  }
+
+  for (let number of fibonacciSeries) {
+    console.log(number);
+  }
+}
+
+fibonacci(10);
+
+
+// print actual pyramid
 function printPyramid(n){
 
   for (let i = 1; i <= n; i++) {
     let pattern = '';
 
-    // Adding spaces before each row to create the pyramid shape
+    // Adding spaces before each row to get the desired pyramid shape
     for (let j = 1; j <= n - i; j++) {
       pattern += ' ';
     }
 
-    // Adding asterisks for the current row
+    // Adding stars for the current row
     for (let k = 1; k <= i; k++) {
       pattern += '* ';
     }
@@ -47,3 +80,5 @@ function printPyramid(n){
 
 const pyramidRes = printPyramid(5);
 console.log(pyramidRes);
+
+
