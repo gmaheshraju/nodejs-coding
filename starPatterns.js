c541b475dff4d6d9355233134ec1d5471c66521e
maheshraju1218@gmail.com <maheshraju1218@gmail.com>
2023-06-11 00:18:58 +0530

    added leetcode easy solution

--- starPatterns.js
+++ starPatterns.js
@@ -82,3 +82,22 @@ const pyramidRes = printPyramid(5);
 console.log(pyramidRes);
 
 
+
+var intersection = function(nums1, nums2) {
+
+  let res = [];
+
+  for(let i=0 ; i< nums1.length; i++) {
+      for (let j=0 ; j<nums2.length; j++) {
+          if(nums1[i] == nums2[j]){
+              if(!res.includes(nums1[i]))
+                 res.push(nums1[i]);
+              break;
+          }
+      }
+  }
+
+  return res;
+};
+
+
